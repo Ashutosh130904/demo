9737d747ef2c2453982e1a049a9a1a93d717401c
Ashutosh pandey <ashutosh130950@gmail.com>
2023-10-26 08:32:47 +0530

    first commit

---
+++ HTML files/script.js
@@ -0,0 +1,15 @@
+// let button=document.querySelector("#btn")
+
+// // button.addEventListener("click",function()
+// // {
+// //     button.innerHTML="Subscribed";
+// //     button.style.color="white";
+// //     button.style.backgroundColor="red";
+// //     button.style.height="40 px";
+   
+// // })
+
+// function btngrow()
+// {
+//     color="white";
+// }
